a63d210bb5eb1a0ac8716ccf3c43314bfddc9ac6
thirotan <thirotanjp@gmail.com>
2016-02-08 17:34:24 +0900

    rm file

--- spec/sinatraapp/app_spec.rb
+++ spec/sinatraapp/app_spec.rb
@@ -6,55 +6,41 @@ describe 'sinatraapp/app.rb' do
   describe 'GET /' do                                                                                                             
     it 'redirect to top page' do                                                                                                                   
       visit '/'
-      expect(page).to be_has_content('新規投稿')
+      expect(page).to have_content('新規投稿')
     end                                                                                                                                      
   end
 
   describe 'POST /add' do
     #投稿失敗
     describe "with invalid attributes" do
-
-      it "does not save the new entry" do
-        skip
+      before do
+        visit '/'
+        within('#post-entry') do
+          fill_in "body", with:""
+          fill_in "name", with:"post test"
+          click_button "POST"
+        end
       end
 
-      it "redirect to top page" do
-        skip
+      it "does not save the new entry" do
+        expect(page).to have_content('本文に何も書いてないです')
       end
     end
 
     #投稿成功
     describe "with valid attributes" do
-      #before do
-      #  visit '/'
-      #  within('#post-entry') do
-      #    fill_in "body", with:"post entry test"
-      #    fill_in "name", with:"post test"
-      #    click_button "POST"
-      #  end
-      #end
+      before do
+        visit '/'
+        within('#post-entry') do
+          fill_in "body", with:"post entry test"
+          fill_in "name", with:"post test"
+          click_button "POST"
+        end
+      end
 
-      #it "redirect to top page" do
-      #  expect(page).to be_has_content('post entry test')
-      #end
+      it "redirect to top page" do
+        expect(page).to be_has_content('post entry test')
+      end
     end
   end                                                                                                                                        
-
-  describe 'GET /entry/:id' do
-    it 'show the new entry of entry page' do
-        skip
-        #expect(last_response).to be_ok                                                                                                         
-        #expect(response).to render_template("/entry/#{id}")                                                                                                        
-    end
-    it 'show the new entry of raw text page' do
-        skip
-        #expect(last_response).to be_ok                                                                                                         
-        #expect(response).to render_template("/entry/#{id}/raw")                                                                                                        
-    end
-    it 'show the new entry of markwodn page' do
-       skip
-        #expect(last_response).to be_ok
-        #expect(response).to render_template("/entry/#{id}/markdown")
-    end
-  end
 end   
